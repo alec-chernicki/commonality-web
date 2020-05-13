4b943e8f0d5067632854e36a2cb6b3ed585dce38
Alec Ortega <aleccortega@gmail.com>
2020-05-12 21:58:40 -0400

    First pass at new

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,46 +19,7 @@ import axios from 'axios';
 import * as yup from 'yup';
 import Head from 'next/head';
 
-const AccentBackground = styled.div`
-  user-select: none;
-  position: absolute;
-  top: 0;
-  bottom: 0;
-  right: 0;
-  left: 55%;
-  background-image: url('/background.svg');
-  background-repeat: repeat;
-  background-size: 1250px;
-  background-position: center;
-  width: 1500px;
-  height: 1000px;
-  z-index: 10;
-  overflow: hidden;
-  display: none;
-
-  @media screen and (min-width: 1000px) {
-    display: block;
-  }
-`;
-
-const AccentSlash = styled.div`
-  position: absolute;
-  background-color: #fff;
-  top: 0;
-  left: -800px;
-  bottom: 0;
-  width: 1000px;
-  transform: skewX(-10deg);
-`;
-
 const Home = () => {
-  const validationSchema = yup.object().shape({
-    email: yup
-      .string()
-      .email('Invalid email')
-      .required('Required'),
-  });
-
   const handleSubmit = (values: { email: string }) => {
     axios.post(
       'https://api.hsforms.com/submissions/v3/integration/submit/7282015/b4ba2fcb-0a8c-4e1a-9bc6-dfdd544d89be',
@@ -74,63 +35,77 @@ const Home = () => {
   };
 
   return (
-    <UIApp use="marketing">
+    <UIApp>
       <Head>
         <title>Commonality</title>
       </Head>
       <UIPage>
-        <UISection fullscreen={true}>
+        <UISection
+          use="dark"
+          backgroundUrl="/commonality-hero.svg"
+          fullWidth={true}
+          fullscreen={true}
+        >
           <UIFlex>
-            <UIBox basis="150px" grow={0}>
-              <UIImage src="/commonality-word.svg" />
+            <UIBox basis="200px" grow={0} shrink={0}>
+              <UIImage src="/commonality-word-light.svg" />
             </UIBox>
           </UIFlex>
-          <UIFlex className="m-top-14">
-            <UIBox basis="600px">
-              <UITag use="info" className="display-inline-block">
-                Coming soon
-              </UITag>
-              <UIHeader level="1">
-                Align every employee toward common goals.
-              </UIHeader>
-
-              <UIHeader level="5" tag="2">
-                <UITextStyle use="info">
-                  Collaboration software that helps every employee make an
-                  impact on your business.
-                </UITextStyle>
+          <UIFlex justify="center" className="m-top-15 p-x-2">
+            <UIBox basis="500px" grow={0} shrink={0} className="text-center">
+              <UIHeader>Build your north star</UIHeader>
+              <UIHeader level="5">
+                Unify data from the software your employees love and align all
+                your teams towards common goals.
               </UIHeader>
               <UIForm
-                validationSchema={validationSchema}
                 onSubmit={handleSubmit}
                 initialValues={{ email: '' }}
-                className="m-top-6"
+                className="m-top-8"
               >
-                <UIFlex align="flex-start">
-                  <UIBox basis={{ sm: '100%', md: '350px' }}>
-                    <UIFormControl name="email" className="m-bottom-0">
-                      <UITextInput
-                        placeholder="Enter your email address"
-                        name="email"
-                      />
-                    </UIFormControl>
+                <UIFlex align="center">
+                  <UIBox grow={2} className="m-right-2">
+                    <UITextInput name="email" />
                   </UIBox>
-                  <UIBox basis={{ sm: '100%', md: 'auto' }}>
-                    <UIButton
-                      type="submit"
-                      className="m-top-4 md:m-top-0 md:m-left-2 sm:display-block"
-                    >
-                      Join waitlist
-                    </UIButton>
+                  <UIBox>
+                    <UIButton>Register</UIButton>
                   </UIBox>
                 </UIFlex>
               </UIForm>
             </UIBox>
           </UIFlex>
         </UISection>
-        <AccentBackground>
-          <AccentSlash />
-        </AccentBackground>
+        <UISection use="neutral">
+          <UIFlex>
+            <UIBox basis="50%"></UIBox>
+            <UIBox basis="400px">
+              <UIHeader level="3">Measureable goals</UIHeader>
+              <p>
+                From OKRS to SMART goals and everything in between, create goals
+                unique caterered to your business. Create measurable results
+                that define success and monitor the progression of those goals
+                against your financial deadlines. Manually update goals or
+                automatically collect data from the tools your employees already
+                love using.
+              </p>
+            </UIBox>
+          </UIFlex>
+        </UISection>
+        <UISection use="light">
+          <UIFlex>
+            <UIBox basis="50%"></UIBox>
+            <UIBox basis="400px">
+              <UIHeader level="3">Top to bottom alignment</UIHeader>
+              <p>
+                Gain true insight into the performance of your business and tie
+                goals from every team back to key company metrics. Instantly
+                view the health of your company and discover areas of
+                opportunity or areas for investment.
+              </p>
+            </UIBox>
+          </UIFlex>
+        </UISection>
+        <UISection use="dark"></UISection>
       </UIPage>
     </UIApp>
   );
